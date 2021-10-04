4710d1a8df04ef2c7d0f1bc148e90fdb7f15ea1f
Axel Mitschi <axel.mitschi@gmail.com>
2021-10-04 15:52:13 +0200

    algo fixed

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 'use-strict';
 
 /* Config */
-const CELL_SIZE = 15;
+const CELL_SIZE = 20;
 
-const GRID_SIZE_X = 40;
-const GRID_SIZE_Y = 40;
+const GRID_SIZE_X = 30;
+const GRID_SIZE_Y = 30;
 
-const IN_SET_X = 3;
-const IN_SET_Y = 3;
+const IN_SET_X = 5;
+const IN_SET_Y = 5;
 
-const BORDER_CANVAS = 3;
+const BORDER_CANVAS = 5;
 
 /* -------------------------------------------- */
 const CANVAS_SIZE_X = (GRID_SIZE_X * CELL_SIZE) + (GRID_SIZE_X * IN_SET_X) + IN_SET_X;
@@ -65,9 +65,23 @@ function drawGrid(grid, ctx, canvas) {
             } else {
                 ctx.fillStyle = '#111';
             }
-            ctx.fillRect(i * (CELL_SIZE + IN_SET_X) + IN_SET_X, j * (CELL_SIZE + IN_SET_Y) + IN_SET_Y, CELL_SIZE, CELL_SIZE);
+            ctx.fillRect(i * (CELL_SIZE + IN_SET_X) + IN_SET_X,
+                         j * (CELL_SIZE + IN_SET_Y) + IN_SET_Y,
+                         CELL_SIZE,
+                         CELL_SIZE);
         }
     }
+}
+
+function debugGrid(grid) {
+    let g = [];
+    for (let i = 0; i < grid.length; i++) {
+        g[i] = [];
+        for (let j = 0; j < grid[i].length; j++) {
+            g[i][j] = grid[i][j].aLife;
+        }
+    }
+    console.table(g);
 }
 
 window.onload = () => {
@@ -75,32 +89,61 @@ window.onload = () => {
     const ctx = canvas.getContext('2d');
 
     let gridCell = createGridCell();
+    drawGrid(gridCell, ctx, canvas);
 
     document.addEventListener('click', e => {
         const mouseX = e.pageX - canvas.offsetLeft;
         const mouseY = e.pageY - canvas.offsetTop;
-        console.clear();
 
         for (let i = 0; i < gridCell.length; i++) {
             for (let j = 0; j < gridCell[i].length; j++) {
                 if (mouseX > (gridCell[i][j].posX + IN_SET_X) + (i * IN_SET_X) + IN_SET_X &&
-                    mouseX < (gridCell[i][j].posX + CELL_SIZE + IN_SET_X) + (i * IN_SET_X) + IN_SET_X &&
+                    mouseX <= (gridCell[i][j].posX + CELL_SIZE + IN_SET_X) + (i * IN_SET_X) + IN_SET_X &&
                     mouseY > (gridCell[i][j].posY + IN_SET_Y) + (j * IN_SET_Y) + IN_SET_Y &&
-                    mouseY < (gridCell[i][j].posY + CELL_SIZE + IN_SET_Y) + (j * IN_SET_Y) + IN_SET_Y) {
+                    mouseY <= (gridCell[i][j].posY + CELL_SIZE + IN_SET_Y) + (j * IN_SET_Y) + IN_SET_Y) {
                         console.log(`Cell(x:${i}, y:${j})`);
-
                         if (gridCell[i][j].aLife) {
                             gridCell[i][j].aLife = false;
-                            drawGrid(gridCell, ctx, canvas);
                         } else {
                             gridCell[i][j].aLife = true;
-                            drawGrid(gridCell, ctx, canvas);
                         }
-
+                        drawGrid(gridCell, ctx, canvas);
                     }
             }
         }
+        
     });
-
-    drawGrid(gridCell, ctx, canvas);
+    
+    document.addEventListener("keypress", e => {
+        if (e.key === 's') {
+            let g = createGridCell();
+            for (let i = 0; i < gridCell.length; i++) {
+                for (let j = 0; j < gridCell[i].length; j++) {
+                    let nbCells = 0;
+                    try {
+                        if (gridCell[i - 1][j - 1].aLife) nbCells++; // 1
+                        if (gridCell[i    ][j - 1].aLife) nbCells++; // 2
+                        if (gridCell[i + 1][j - 1].aLife) nbCells++; // 3
+                        if (gridCell[i - 1][j    ].aLife) nbCells++; // 4
+                        if (gridCell[i + 1][j    ].aLife) nbCells++; // 5
+                        if (gridCell[i - 1][j + 1].aLife) nbCells++; // 6
+                        if (gridCell[i    ][j + 1].aLife) nbCells++; // 7
+                        if (gridCell[i + 1][j + 1].aLife) nbCells++; // 8
+                    } catch (error) {
+                        
+                    }
+                    if (!gridCell[i][j].aLife && nbCells === 3) {
+                        g[i][j].aLife = true;
+                        
+                    } else if (gridCell[i][j].aLife && nbCells === 3 || gridCell[i][j].aLife && nbCells === 2) {
+                        g[i][j].aLife = true;
+                    } else {
+                        g[i][j].aLife = false;
+                    }
+                }
+            }
+            gridCell = [...g];
+            drawGrid(gridCell, ctx, canvas);
+        }
+    });
 }
